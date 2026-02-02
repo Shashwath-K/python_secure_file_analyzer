9a68f756191db8b7b0bc757fe562324cf7ad1cb7
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-02-02 10:46:40 +0530

    application entry point has been initialized

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-# Application entry point
+from app.config import (
+    APP_NAME,
+    APP_VERSION,
+    ensure_directories,
+)
+from app.ui.gradio_app import create_interface
+
+
+def main() -> None:
+    """
+    Application entry point.
+    """
+
+    ensure_directories()
+
+    interface = create_interface()
+
+    interface.launch(
+        server_name="127.0.0.1",
+        server_port=7860,
+        show_api=False,
+        share=False
+    )
+
+
+if __name__ == "__main__":
+    print(f"{APP_NAME} | Version {APP_VERSION}")
+    main()
